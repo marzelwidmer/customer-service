af7301b8d0c4a8b03a89c20a8544f882f97f933c
Marcel Widmer <marzelwidmer@gmail.com>
2019-09-16 17:48:16 +0200

    Polishing.

--- foobar.sh
+++ foobar.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-# Author:Andrey Nikishaev
 echo "CHANGELOG"
 echo ----------------------
-git tag -l --sort=v:refname |  while read TAG ; do
+git tag -l  | while read TAG ; do
     echo
     if [ $NEXT ];then
+        echo [$TAG]
         echo [$NEXT]
     else
-        echo "[Current]"
+        echo [$TAG]
     fi
     GIT_PAGER=cat git log --no-merges --format=" * %s" $TAG..$NEXT
     NEXT=$TAG
@@ -16,3 +16,72 @@ FIRST=$(git tag -l --sort=v:refname | head -1)
 echo
 echo [$FIRST]
 GIT_PAGER=cat git log --no-merges --format=" * %s" $FIRST
+
+#echo "CHANGELOG"
+#echo ----------------------
+#git tag -l --sort=v:refname | while read TAG ; do
+#    echo
+#    if [ $NEXT ];then
+#        echo [$NEXT]
+#        echo tag: $TAG
+#        echo next: $NEXT
+#        git log --no-merges --format=" * %s" $TAG..$NEXT
+#        git log  $TAG..$NEXT --pretty=format:"  * %s"
+#    else
+#        echo "[Current]"
+#    fi
+#    NEXT=$TAG
+#done
+
+#git config --global alias.plog "log --graph --pretty=format:'%h -%d %s %n' --abbrev-commit --date=relative --branches"
+#git log `git describe --tags --abbrev=0`..HEAD --pretty=format:"  * %s"
+
+#git tag -l --sort=v:refname | while read TAG ; do
+#    echo
+#    if [ $NEXT ];then
+#        echo [$NEXT]
+#        for COMMIT in $COMMITS; do
+#            # Get the subject of the current commit
+#            SUBJECT=$(git log -1 ${COMMIT} --pretty=format:"%s" | grep -E '(feature:|minor:|feat:|chore:)')
+#            echo $SUBJECT
+#        done
+#    fi
+#    NEXT=$TAG
+#done
+
+
+
+#echo "CHANGELOG2"
+#echo ----------------------
+## Get a list of all tags in reverse order
+## Assumes the tags are in version format like v1.2.3
+#GIT_TAGS=$(git tag -l --sort=-version:refname)
+#
+## Make the tags an array
+#TAGS=($GIT_TAGS)
+#LATEST_TAG=${TAGS[0]}
+#PREVIOUS_TAG=${TAGS[1]}
+
+
+# If you want to specify your own two tags to compare, uncomment and enter them below
+# LATEST_TAG=v0.23.1
+# PREVIOUS_TAG=v0.22.0
+
+# Get a log of commits that occured between two tags
+# We only get the commit hash so we don't have to deal with a bunch of ugly parsing
+# See Pretty format placeholders at https://git-scm.com/docs/pretty-formats
+#COMMITS=$(git log $PREVIOUS_TAG..$LATEST_TAG --pretty=format:"%H")
+
+
+#echo $LATEST_TAG
+#echo $PREVIOUS_TAG
+#echo $COMMITS
+
+
+# Loop over each commit and look for merged pull requests
+#for COMMIT in $COMMITS; do
+#	# Get the subject of the current commit
+#	SUBJECT=$(git log -1 ${COMMIT} --pretty=format:"%s" | grep -E '(feature:|minor:|feat:|chore:)')
+#    echo $SUBJECT
+#done
+
--- jenkins/verify.sh
+++ jenkins/verify.sh
@@ -23,18 +23,25 @@ VNUM3=${VERSION_BITS[2]}
 MAJOR_COUNT_OF_COMMIT_MSG=`git log $GIT_REV_LIST..$GIT_REV_LAST --pretty=%B | egrep -c '^(breaking:|major:|BREAKING CHANGE:)'`
 MINOR_COUNT_OF_COMMIT_MSG=`git log $GIT_REV_LIST..$GIT_REV_LAST --pretty=%B | egrep -c '^(feature:|minor:|feat:)'`
 PATCH_COUNT_OF_COMMIT_MSG=`git log $GIT_REV_LIST..$GIT_REV_LAST --pretty=%B | egrep -c '^(fix:|patch:|docs:|style:|refactor:|perf:|test:|chore:)'`
+
+# MAJOR 0.0.1
 if [ $PATCH_COUNT_OF_COMMIT_MSG -gt 0 ]; then
     VNUM3=$((VNUM3+1))
 fi
+
+# MAJOR 0.1.0
 if [ $MINOR_COUNT_OF_COMMIT_MSG -gt 0 ]; then
     VNUM2=$((VNUM2+1))
     VNUM3=0
 fi
+
+# MAJOR 1.0.0
 if [ $MAJOR_COUNT_OF_COMMIT_MSG -gt 0 ]; then
     VNUM1=$((VNUM1+1))
     VNUM2=0
     VNUM3=0
 fi
+
 # count all commits for a branch
 GIT_COMMIT_COUNT=`git rev-list --count HEAD`
 #create new tag
